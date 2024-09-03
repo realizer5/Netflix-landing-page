b3798d7d7f83840867700ae83b641ffa63bb2536
realizer <ipreet35232@gmail.com>
2024-09-03 09:08:34 +0530

    pushing commits to github repo

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const inputMail = document.getElementById('inputMail');
+const inputMail = document.getElementById("inputMail");
+const errorTextDiv = document.querySelector(".error-text-div");
+const errorText = document.getElementById("errorText");
 
-inputMail.addEventListener('input', e=>{    
-    const inputData = e.target.value
-    if(inputData.length < 5){
-        // Email is required.
-    } else{
-        // Please enter a valid email address.
-    }
-    const inputValid = inputMail.checkValidity()
-    if(inputValid===true && inputData.length > 0){
-        inputMail.style.borderColor = "rgb(43,184,113)";
-    }else{
-        inputMail.style.borderColor = "rgb(235,57,66)";
-    }
-})
+inputMail.addEventListener("input", (e) => {
+  const inputData = e.target.value;
+  if (inputData.length < 5) {
+    // Email is required.
+    errorText.innerText = `Email is required.`;
+  } else {
+    // Please enter a valid email address.
+    errorText.innerText = `Please enter a valid email address.`;
+  }
+  const inputValid = inputMail.checkValidity();
+  if (inputValid === true && inputData.length > 0) {
+      inputMail.style.borderColor = "rgb(43,184,113)";
+      errorTextDiv.style.display = "none";
+  } else {
+    inputMail.style.borderColor = "rgb(235,57,66)";
+    errorTextDiv.style.display = "flex";
+  }
+});
