197ec04d4844298b91ef2c4f2c3141a9a69502fd
realizer <ipreet35232@gmail.com>
2024-09-12 10:04:10 +0530

    added trending cards and removed select cuz netflix removed it

--- script.js
+++ script.js
@@ -13,10 +13,24 @@ inputMail.addEventListener("input", (e) => {
   }
   const inputValid = inputMail.checkValidity();
   if (inputValid === true && inputData.length > 0) {
-      inputMail.style.borderColor = "rgb(43,184,113)";
-      errorTextDiv.style.display = "none";
+    inputMail.style.borderColor = "rgb(43,184,113)";
+    errorTextDiv.style.display = "none";
   } else {
     inputMail.style.borderColor = "rgb(235,57,66)";
     errorTextDiv.style.display = "flex";
   }
 });
+
+// netflix removed this feature
+
+// const region = document.getElementById("region");
+// const contentType = document.getElementById("contentType");
+
+// region.addEventListener("change", (e) => {
+//   value = region.value;
+//   if (value === "india") {
+//     contentType.innerHTML = `<option value="">Movies</option><option value="">Tv Shows</option>`;
+//   } else {
+//     contentType.innerHTML = `<option value="">Movies - English</option><option value="">Movies - Other languages</option><option value="">Tv Shows - English</option><option value="">Tv Shows - Other languages</option>`;
+//   }
+// });
